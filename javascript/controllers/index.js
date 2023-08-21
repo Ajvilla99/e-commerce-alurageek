ebadc769f6474a1c0f866c418c43314dc98a3e84
Abrahan <ajvilla1999@gmail.com>
2023-08-21 10:09:57 -0500

    second commit 02

--- javascript/controllers/index.js
+++ javascript/controllers/index.js
@@ -1,5 +1,25 @@
 import { clientServices } from "../services/product-services.js";
 
+const menu = document.querySelector('.menu_mobile')
+const navbarActive = document.querySelector('.nav_container')
+const spanTop = document.querySelector('.line_top')
+const spanMid = document.querySelector('.line_mid')
+const spanBot = document.querySelector('.line_bot')
+const gift = document.querySelector('.gift')
+const giftClose = document.querySelector('.close_gift')
+
+menu.addEventListener('click', () => {
+    spanTop.classList.toggle('active_top')
+    spanMid.classList.toggle('active_mid')
+    spanBot.classList.toggle('active_bot')
+    navbarActive.classList.toggle('active_navbar')
+})
+
+
+giftClose.addEventListener('click', () => {
+    gift.style.display = 'none'
+})
+
 
 const menuActive = document.querySelector('.menu_mobile');
 const navMenu = document.querySelector('.nav__menu');
--- javascript/controllers/view-product.js
+++ javascript/controllers/view-product.js
@@ -1,5 +1,26 @@
 import { clientServices } from "../services/product-services.js";
 
+const menu = document.querySelector('.menu_mobile')
+const navbarActive = document.querySelector('.nav_container')
+const spanTop = document.querySelector('.line_top')
+const spanMid = document.querySelector('.line_mid')
+const spanBot = document.querySelector('.line_bot')
+const gift = document.querySelector('.gift')
+const giftClose = document.querySelector('.close_gift')
+
+menu.addEventListener('click', () => {
+    spanTop.classList.toggle('active_top')
+    spanMid.classList.toggle('active_mid')
+    spanBot.classList.toggle('active_bot')
+    navbarActive.classList.toggle('active_navbar')
+})
+
+
+giftClose.addEventListener('click', () => {
+    gift.style.display = 'none'
+})
+
+
 const urlImage = document.querySelector('[data-image]')
 const name = document.querySelector('[data-name]')
 const description = document.querySelector('[data-description]')
